0200bdf639ae380f4f8d5b99ac5e3af7da5dda5b
Moeif Studio <moeif@outlook.com>
2021-08-12 14:06:06 +0800

    修改机器人队列

--- src/robottable.rs
+++ src/robottable.rs
@@ -10,26 +10,47 @@ pub struct RobotRecord {
 }
 
 pub struct RobotTable {
-    pub robot_vec: VecDeque<RobotRecord>,
+    pub robot_deque_array: [VecDeque<RobotRecord>; 4],
+    pub get_index: usize,
+    pub back_index: usize,
 }
 
 impl RobotTable {
     pub fn new() -> Self {
         let file = std::fs::File::open("./configs/robot_info.csv").unwrap();
         let mut rdr = csv::Reader::from_reader(file);
-        let mut robot_vec = VecDeque::new();
+
+        let mut robot_deque_array: [VecDeque<RobotRecord>; 4] = [
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+        ];
+
+        let mut record_index = 0;
+
         for result in rdr.deserialize() {
             let record: RobotRecord = result.unwrap();
-            robot_vec.push_back(record);
+            let deque_index = record_index % robot_deque_array.len();
+            robot_deque_array[deque_index].push_back(record);
+            record_index += 1;
         }
 
-        Self { robot_vec }
+        Self {
+            robot_deque_array,
+            get_index: 0,
+            back_index: 0,
+        }
     }
 
     pub fn get_id_name(&mut self) -> (String, String) {
-        if let Some(robot) = self.robot_vec.pop_front() {
+        if let Some(robot) =
+            self.robot_deque_array[self.get_index % self.robot_deque_array.len()].pop_front()
+        {
+            self.get_index += 1;
             (robot.id, robot.name)
         } else {
+            self.get_index += 1;
             let mut rng = rand::thread_rng();
             let num: i32 = rng.gen_range(100000..999990);
             let id = Uuid::new_v4().to_simple().to_string();
@@ -41,7 +62,7 @@ impl RobotTable {
 
     pub fn back_id_name(&mut self, id: String, name: String) {
         let robot = RobotRecord { id, name };
-
-        self.robot_vec.push_front(robot);
+        self.robot_deque_array[self.back_index % self.robot_deque_array.len()].push_front(robot);
+        self.back_index += 1;
     }
 }
